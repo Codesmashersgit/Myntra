3aa531b85e5dd765a7c65002c0bddcf2f8b4db34
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2025-09-27 22:00:37 +0530

    Added some functionality in Cart

--- Frontend/e-app/src/components/section/Cart.jsx
+++ Frontend/e-app/src/components/section/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from '../Nav'
+import { Link } from 'react-router-dom'
 
 const Cart = ({ dark, toggle, showdisplay,showbag}) => {
   return (
@@ -11,11 +12,16 @@ const Cart = ({ dark, toggle, showdisplay,showbag}) => {
         showbag={!showbag}
       />
       <div
-        className={`h-screen w-screen flex flex-col justify-center items-center pt-7 ${
+        className={`h-screen w-screen flex flex-col gap-3 justify-center items-center pt-7 ${
           dark ? 'bg-black text-white' : 'bg-white text-black'
         } transition-all duration-1000 ease-in-out`}
       >
-        Cart
+        <img src='https://constant.myntassets.com/checkout/assets/img/empty-bag.webp' className='h-[150px]'/>
+        <p className='font-[sk] text-center'>Hey, it feels so light!</p>
+        <p className='font-light text-center'>There is nothing in your bag. Let's add some items.</p>
+        <Link to="/wishlist">
+        <button className='text-[#ff3f6c] border border-[#ff3f6c] font-[sk] p-2'>ADD ITEMS FROM WISHLIST</button>
+        </Link>
       </div>
     </>
   )
